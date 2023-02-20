529136d22cf86b8d285c3d3de1dd700d0fbee736
PelissierCombescure <43579644+PelissierCombescure@users.noreply.github.com>
2023-02-20 11:07:46 +0100

    version 20-02 a 11h

--- Code/fonctions.js
+++ Code/fonctions.js
@@ -1,3 +1,27 @@
+// Variable global
+// bouton
+function init_variable_fonction(dict_boutons, dict_imgs ){
+    // bouton
+    scale_bouton = 0.7
+    h_bouton = scale_bouton*dict_boutons["choix_pose"].height
+    w_bouton = scale_bouton*dict_boutons["choix_pose"].width
+    // fleche 
+    scale_fleche = 0.12
+    dy = 20
+    a = scale_fleche*dict_imgs["gauche"].height
+    b = scale_fleche*dict_imgs["gauche"].width
+    // Couleur 
+    alpha_bloque = 0.6
+    // Rayon 
+    R = 2.5
+    // progress bar
+    x_progress_bar = W_3D
+    y_progress_bar = 0
+    w_progress_bar = 100
+    h_progress_bar = 20
+}
+
+
 function new_image(src) {
     img = new Image()
     img.src = src
@@ -39,12 +63,53 @@ function draw_contour(originex, originey, largeur, hauteur, couleur){
     ctx.lineWidth = 1
 }
 
+function bloquer_pose(L_poses){
+    for (p=0; p<L_poses.length; p++){
+        idx_i_p = L_poses[p][3]
+        idx_j_p = L_poses[p][4]
+        // Si on est en train de voir une pose déjà choisie
+        if (idx_i== idx_i_p && idx_j==idx_j_p){
+            draw_rectangle(0,0, W_3D, H_3D, "rgb(0, 0, 0)", alpha_bloque)
+            ctx.font = "bold 18pt Courier";
+            ctx.strokeStyle = "rgb(255, 255, 255)" // Pour que le contour soit rouge
+            ctx.fillStyle = "rgb(255, 255, 255)"
+            ctx.fillText("Pose déjà sélectionnée", 10, H_3D)
+        }
+    }
+}
+
+function progress_bar(N_tache, N_mesh){
+    if (N_tache<=N_mesh){
+        // background
+        draw_rectangle(x_progress_bar, y_progress_bar, w_progress_bar, h_progress_bar, "rgb(255,255,255)", 1)
+        // bar 
+        w_bar = ((N_tache-1)/N_mesh)*w_progress_bar
+        draw_rectangle(x_progress_bar, y_progress_bar, w_bar, h_progress_bar, "rgb(0,0,255)", 1)
+        // numero de tache 
+        ctx.strokeStyle = "rgb(255, 255, 255)" // Pour que le contour soit rouge
+        ctx.fillStyle = "rgb(255, 255, 255)" // Pour que l'intérieur soit bleu
+        ctx.font = "bold 18pt Courier";
+        ctx.fillText((num_tache)+"/"+(N_mesh), x_progress_bar+w_progress_bar+10, h_progress_bar)
+    }
+}
+
+function pose_deja_choisie(L_poses, i_choix, j_choix){
+    deja_choisie = false
+    for (p=0; p<L_poses.length; p++){
+        idx_i_p = L_poses[p][3]
+        idx_j_p = L_poses[p][4]
+        // Si on est en train de voir une pose déjà choisie
+        if (i_choix == idx_i_p && j_choix==idx_j_p){
+            deja_choisie = true
+        }
+    }
+}
+
+
 ///////////////////////////////////////////////////////////////
 ///////////////////// FLECHE 
 
-function afficher_fleche(dict_imgs, dy, scale_fleche){
-    a = scale_fleche*dict_imgs["gauche"].height
-    b = scale_fleche*dict_imgs["gauche"].width
+function afficher_fleche(dict_imgs){
     // image, posx, posy, W, H
     ctx.drawImage(dict_imgs["bas"], W_3D*0.35, H_3D+dy+b+a, a, b)
     ctx.drawImage(dict_imgs["haut"], W_3D*0.35, H_3D+dy, a, b)
@@ -76,38 +141,34 @@ function survol_fleche(){
 }
 
 // MAJ de which_clicked avec le nom de la fleche clickée
-function get_clicked_fleche(dict_imgs){
-    a = scale_fleche*dict_imgs["gauche"].height
-    b = scale_fleche*dict_imgs["gauche"].width
+function get_clicked_fleche(){
     // Fleche GAUCHE
     if (clicked && xyMouseMove.x >= W_3D*0.35-b  && xyMouseMove.x <= W_3D*0.35 && xyMouseMove.y > H_3D+dy+b && xyMouseMove.y < H_3D+dy+b+a ){
-        which_clicked = 'gauche'
+        which_clicked_fleche = 'gauche'
     }
     // Fleche DROITE
     if (clicked && xyMouseMove.x >= W_3D*0.35+a  && xyMouseMove.x <= W_3D*0.35+a+b && xyMouseMove.y > H_3D+dy+b && xyMouseMove.y < H_3D+dy+b+a ){
-        which_clicked = 'droite'
+        which_clicked_fleche = 'droite'
     }
     // Fleche HAUT
     if (clicked && xyMouseMove.x >= W_3D*0.35  && xyMouseMove.x <= W_3D*0.35+a && xyMouseMove.y > H_3D+dy && xyMouseMove.y < H_3D+dy+b ){
     // l'image devient verte 
-        which_clicked = 'haut'
+        which_clicked_fleche = 'haut'
     }
     // Fleche BAS
     if (clicked && xyMouseMove.x >=  W_3D*0.35  && xyMouseMove.x <=  W_3D*0.35+a && xyMouseMove.y > H_3D+dy+b+a && xyMouseMove.y < H_3D+dy+b+a+b ){
         // l'image devient verte 
-        which_clicked = 'bas'
+        which_clicked_fleche = 'bas'
     }
 }
 
-function traitement_fleche(dict_imgs){
-    a = scale_fleche*imgs["gauche"].height
-    b = scale_fleche*imgs["gauche"].width
+function traitement_fleche(){
     // Survol des fleches avec la souris
     survol_fleche()
     // click sur une fleche
-    get_clicked_fleche(dict_imgs)
-    console.log(which_clicked)
-    switch (which_clicked){
+    get_clicked_fleche()
+    //console.log(which_clicked_fleche)
+    switch (which_clicked_fleche){
         case'gauche' :
             console.log("deplacement G")
             idx_i = (idx_i+1)%8
@@ -128,7 +189,7 @@ function traitement_fleche(dict_imgs){
             break;
         }
      
-    R = 2.5
+    //R = 2.5
     theta = 2*Math.PI * ( (2/8)*(idx_j==0) + (1/8)*(idx_j==1) + (-1/8)*(idx_j==3) + (-2/8)*(idx_j==4))
     delta = 2*Math.PI * (idx_i/8)
     //console.log(theta, delta)
@@ -138,79 +199,133 @@ function traitement_fleche(dict_imgs){
 
 
     if (clicked){
-        console.log("angles: ",theta, delta)
+        //console.log("affichage angles: ",theta, delta)
     }
 
 }
 
 ///////////////////////////////////////////////////////////////
 ///////////////////// BOUTON
-function afficher_bouton(dict_boutons, dict_imgs, scale_bouton, scale_fleche){
-    h_bouton = scale_bouton*dict_boutons["choix_pose"].height
-    w_bouton = scale_bouton*dict_boutons["choix_pose"].width
-    ecart = 80 // entre les fleches et le bouton
-
-    a = scale_fleche*dict_imgs["gauche"].height
-    b = scale_fleche*dict_imgs["gauche"].width
+function afficher_bouton(dict_boutons){
+    ecart = 40 // entre les fleches et le bouton
     // image, posx, posy, W, H
-    ctx.drawImage(dict_boutons["choix_pose"], W_3D*0.35+a+b+ecart, H_3D+dy+b, w_bouton, h_bouton)
-    ctx.drawImage(dict_boutons["retirer"], W_3D*0.35+a+b+ecart*4, H_3D+dy+b, w_bouton, h_bouton)
+    ctx.drawImage(dict_boutons["choix_pose"], W_3D*0.35+a+2*b+ecart, H_3D+dy+b, w_bouton, h_bouton)
+    ctx.drawImage(dict_boutons["retirer"], W_3D*0.35+a+2*b+ecart*7, H_3D+dy+b, w_bouton, h_bouton)
+    ctx.drawImage(dict_boutons["reinitialiser"], W_3D*0.35+a+2*b+ecart*7, H_3D+dy+b+ecart*2, w_bouton, h_bouton)
+    ctx.drawImage(dict_boutons["valider"], W_3D*0.35+a+2*b+ecart, H_3D+dy+b+ecart*2, w_bouton, h_bouton)
 }
 
 function survol_bouton(){
     // Choisir le pt de vue
-    if (xyMouseMove.x >= W_3D*0.35+a+b+ecart  && xyMouseMove.x <= W_3D*0.35+a+b+ecart+w_bouton && xyMouseMove.y > H_3D+dy+b && xyMouseMove.y < H_3D+dy+b+h_bouton ){
-        draw_contour(W_3D*0.35+a+b+ecart, H_3D+dy+b, w_bouton, h_bouton, "rgb(255, 0, 255)")
+    if (xyMouseMove.x >= W_3D*0.35+a+2*b+ecart  && xyMouseMove.x <= W_3D*0.35+a+2*b+ecart+w_bouton && xyMouseMove.y > H_3D+dy+b && xyMouseMove.y < H_3D+dy+b+h_bouton ){
+        draw_contour(W_3D*0.35+a+2*b+ecart, H_3D+dy+b, w_bouton, h_bouton, "rgb(255, 0, 255)")
     }
     // Retirer 
-    if (xyMouseMove.x >= W_3D*0.35+a+b+ecart*4  && xyMouseMove.x <= W_3D*0.35+a+b+ecart*4+w_bouton && xyMouseMove.y > H_3D+dy+b && xyMouseMove.y < H_3D+dy+b+h_bouton ){
-        draw_contour(W_3D*0.35+a+b+ecart*4, H_3D+dy+b, w_bouton, h_bouton, "rgb(255, 0, 255)")
+    if (xyMouseMove.x >= W_3D*0.35+a+2*b+ecart*7  && xyMouseMove.x <= W_3D*0.35+a+2*b+ecart*7+w_bouton && xyMouseMove.y > H_3D+dy+b && xyMouseMove.y < H_3D+dy+b+h_bouton ){
+        draw_contour(W_3D*0.35+a+2*b+ecart*7, H_3D+dy+b, w_bouton, h_bouton, "rgb(255, 0, 255)")
+    }
+    // Reintialiser
+    if (xyMouseMove.x >= W_3D*0.35+a+2*b+ecart*7  && xyMouseMove.x <= W_3D*0.35+a+2*b+ecart*7+w_bouton && xyMouseMove.y > H_3D+dy+b+ecart*2 && xyMouseMove.y < H_3D+dy+b+ecart*2+h_bouton ){
+        draw_contour(W_3D*0.35+a+2*b+ecart*7, H_3D+dy+b+ecart*2, w_bouton, h_bouton, "rgb(255, 0, 255)")
+    }
+    // Valider
+    if (xyMouseMove.x >= W_3D*0.35+a+2*b+ecart  && xyMouseMove.x <= W_3D*0.35+a+2*b+ecart+w_bouton && xyMouseMove.y > H_3D+dy+b+ecart*2 && xyMouseMove.y < H_3D+dy+b+ecart*2+h_bouton ){
+        draw_contour(W_3D*0.35+a+2*b+ecart, H_3D+dy+b+ecart*2, w_bouton, h_bouton, "rgb(255, 0, 255)")
     }
 }
 
-function get_clicked_bouton(dict_boutons){
-    h_bouton = scale_bouton*dict_boutons["choix_pose"].height
-    w_bouton = scale_bouton*dict_boutons["choix_pose"].width
-
-    if (clicked && xyMouseMove.x >= W_3D*0.35+a+b+ecart  && xyMouseMove.x <= W_3D*0.35+a+b+ecart+w_bouton && xyMouseMove.y > H_3D+dy+b && xyMouseMove.y < H_3D+dy+b+h_bouton ){
-        which_clicked = "bouton_pose"
+function get_clicked_bouton(){
+    if (clicked && xyMouseMove.x >= W_3D*0.35+a+2*b+ecart  && xyMouseMove.x <= W_3D*0.35+a+2*b+ecart+w_bouton && xyMouseMove.y > H_3D+dy+b && xyMouseMove.y < H_3D+dy+b+h_bouton ){
+        which_clicked_bouton = "bouton_pose"
     }
-    if (clicked && xyMouseMove.x >= W_3D*0.35+a+b+ecart*4  && xyMouseMove.x <= W_3D*0.35+a+b+ecart*4+w_bouton && xyMouseMove.y > H_3D+dy+b && xyMouseMove.y < H_3D+dy+b+h_bouton ){
-        which_clicked = "bouton_retirer"
+    if (clicked && xyMouseMove.x >= W_3D*0.35+a+2*b+ecart*7  && xyMouseMove.x <= W_3D*0.35+a+2*b+ecart*7+w_bouton && xyMouseMove.y > H_3D+dy+b && xyMouseMove.y < H_3D+dy+b+h_bouton ){
+        which_clicked_bouton = "bouton_retirer"
     }
-
+    if (clicked && xyMouseMove.x >= W_3D*0.35+a+2*b+ecart*7  && xyMouseMove.x <= W_3D*0.35+a+2*b+ecart*7+w_bouton && xyMouseMove.y > H_3D+dy+b+ecart*2 && xyMouseMove.y < H_3D+dy+b+ecart*2+h_bouton ){
+        which_clicked_bouton = "bouton_reinitialiser"
+    }
+    if (clicked && xyMouseMove.x >= W_3D*0.35+a+2*b+ecart  && xyMouseMove.x <= W_3D*0.35+a+2*b+ecart+w_bouton && xyMouseMove.y > H_3D+dy+b+ecart*2 && xyMouseMove.y < H_3D+dy+b+ecart*2+h_bouton ){
+        which_clicked_bouton = "bouton_valider"
+    }
 }
 
-function traitement_bouton(dict_boutons){
+function traitement_bouton(){
     // si on survol, on a les contours qui apparaissent
     survol_bouton()
     // si on click
-    get_clicked_bouton(dict_boutons)
-    switch (which_clicked){
+    get_clicked_bouton()
+    switch (which_clicked_bouton){
         // bouton valider pose : on rajoute le bouton à la liste
         case 'bouton_pose':
-            console.log('bouton ajout pose')
-            liste_poses.push(['choix'+(nb_choix_fait+1),theta, delta, idx_i, idx_j]) 
-            nb_choix_fait = nb_choix_fait+1
+            console.log('bouton ajout pose', nb_choix_fait)
+            // on regarde si la pose sélectionnée n'a pas déjà été choisie avant
+            pose_deja_choisie(liste_poses, idx_i, idx_j)
+            if (deja_choisie){console.log("Cette pose a déjà été sélectionnée.")}
+            // plus de choix possible
+            if (nb_choix_fait == nb_choix_demande) {console.log("Tu as déjà fait tes "+nb_choix_demande+" choix.")}
+            // si on a pas encore choisie toutes nos poses, on peut en ajouter
+            if (nb_choix_fait < nb_choix_demande && !(deja_choisie)){
+                liste_poses.push(['choix'+(nb_choix_fait+1),theta, delta, idx_i, idx_j, new Date().getTime() - time_click]) 
+                time_click = new Date().getTime()
+                nb_choix_fait = nb_choix_fait+1 
+                ctxMin.drawImage(cc, 0, 0)
+            }
+            
             break;
+
         case 'bouton_retirer':
             console.log('bouton retirer')
             // il y a des poses à retirer
-            if (liste_poses.length > 0){liste_poses.pop()} 
+            if (liste_poses.length > 0){
+                liste_poses.pop()
+                nb_choix_fait = nb_choix_fait-1} 
             // S'il n'y a pas de pose choisie
             else {console.log("Il n'y a pas de pose à retirer.")}
             break;
 
-    }
+        case 'bouton_reinitialiser':
+            console.log('bouton retirer')
+            time_click = new Date().getTime()
+            liste_poses = []
+            nb_choix_fait = 0
+            break;
 
-    //if (clicked){console.log("liste des poses : "+liste_poses)}
-
-    // Si on appuie sur valider
-    // verifier que listes_poeses.length == nb_choix_demande
-    // choix_courant['choix_poses'] = liste_poses
-    // choix['tache_N'+N_tache] = choix_courant
-    // choix_courant = {}
-    // listes_poses = []
-    // N_tache = N_tache+1
-    
+        case 'bouton_valider':
+            console.log('bouton valider', num_tache)
+            if (num_tache == nb_mesh){
+                console.log("c'est la fin")
+                num_tache = num_tache+1}
+            if (nb_choix_demande == liste_poses.length && num_tache < nb_mesh){
+                console.log('au suivant')
+                choix_courant['choix_poses'] = liste_poses
+                choix['tache_N'+num_tache] = choix_courant
+                // RAZ
+                choix_courant = {}
+                liste_poses = []
+                nb_choix_fait = 0
+                // Mesh suivant 
+                indice_mesh = indice_mesh + 1
+                num_tache = num_tache+1
+                setUp_3D(indice_mesh)
+            }
+            if (nb_choix_demande < liste_poses.length){
+                console.log("Tu n'as pas fait tes 3 choix")
+            }
+            else{
+                console.log("PBL bouton valider")
+            }
+            break;
+    }    
 }
+
+function afficher_recap(dict_imgs){
+    for (p = 0; p<liste_poses.length; p++){
+        // si on survole la pieme image
+        ctx.drawImage(dict_imgs['tmp'], W_3D+100, (p+1)*100, dict_imgs['tmp'].width*scale_fleche, dict_imgs['tmp'].height*scale_fleche)
+    }
+    //ctx.drawImage(canvasMin, 900, 100, 100, 100)
+    ctx.drawImage(cc, 0, 0, 800, 800, 900, 100, 800, 800)
+
+}
+
+
--- Code/interface3D.js
+++ Code/interface3D.js
@@ -4,22 +4,18 @@
 // initialisation des variables
 init_variable()
 // initialisation du canvas : load des images 
+setUp_3D(indice_mesh)
 init()
 // action
 animate()
+
 //////////////////////////////////////////////////////////////
 function init_variable(){
-  // Fleche
-  scale_fleche = 0.12
-  dy = 20
-
-  // Boutons
-  scale_bouton = 0.7
-  
-  // SOURIS
+   // SOURIS
   // gestion de la souris : pour savoir si on a clické et sur quelle image on a clické
   clicked = false
-  which_clicked = -1
+  which_clicked_bouton = -1
+  which_clicked_fleche = -1
 
   // TEMPS
   // pour avoir un délai après le click
@@ -31,6 +27,7 @@ function init_variable(){
   // DATA github
   indice_mesh = 0
   mesh_courant = "nope"
+  nb_mesh = 3
   // Indices angles
   idx_i = 0
   idx_j = 2 // Fleche H et B : 0 --> pi/2; 1 --> pi/4; 2 --> 0(en face); 3 --> -pi/4; 4 --> -pi/2
@@ -44,79 +41,28 @@ function init_variable(){
   nb_choix_fait = 0
   // nombre de pose qu'on demande de choisir
   nb_choix_demande = 3
+  // Numero de la tache courant --> il y en a autant que de mesh à voir
+  num_tache = 1
 
 
   // Fenetre 3D
   W_3D = window.innerWidth*0.65
   H_3D = window.innerHeight*0.65
-  // Rayon 
-  R = 2.5
+
 }
 
-function init(){
+function setUp_3D(idx_mesh){
+  idx_i = Math.floor(Math.random()*8)
+  idx_j = Math.floor(Math.random()*5)
 
-  // document.addEventListener("keydown", function(event) {
-  //   if (event.code == 37) {
-  //     console.log("deplacement K-G")
-  //     idx_i = (idx_i+1)%8
-
-  //   } else if (event.code == 39) {
-  //     console.log("deplacement K-D")
-  //     idx_i = (idx_i+7)%8
-
-  //   } else if (event.code == 40) {
-  //     console.log("deplacement K-B")
-  //     idx_j = Math.min(idx_j+1,4) 
-
-  //   } else if (event.code == 38) {
-  //     console.log("deplacement K-H")  
-  //     idx_j = Math.max(idx_j-1,0)
-  
-  //   }
-  //   theta = 2*Math.PI * ( (2/8)*(idx_j==0) + (1/8)*(idx_j==1) + (-1/8)*(idx_j==3) + (-2/8)*(idx_j==4)); delta = 2*Math.PI * (idx_i/8)
-  //   console.log("angles: ",theta, delta)
-
-  // })
-
-  document.addEventListener("keydown", function(event) {
-    switch (event.key){
-      case  'ArrowLeft': 
-        console.log("deplacement K-G")
-        idx_i = (idx_i+1)%8
-        break;
-      case 'ArrowRight' :
-        console.log("deplacement K-D")
-        idx_i = (idx_i+7)%8
-        break;
-      case 'ArrowDown' :
-        console.log("deplacement K-B")
-        idx_j = Math.min(idx_j+1,4) 
-        break;
-      case 'ArrowUp' :
-        console.log("deplacement K-H")  
-        idx_j = Math.max(idx_j-1,0)
-        break;
-      case 'Enter' :
-        console.log('bouton ajout pose')
-        liste_poses.push(['choix'+(nb_choix_fait+1),theta, delta, idx_i, idx_j]) 
-        nb_choix_fait = nb_choix_fait+1
-        break;
-      case 'Backspace':
-        console.log('bouton retirer')
-        // il y a des poses à retirer
-        if (liste_poses.length > 0){liste_poses.pop()} 
-        // S'il n'y a pas de pose choisie
-        else {console.log("Il n'y a pas de pose à retirer.")}
-        break;
-
-    }
-
-    //theta = 2*Math.PI * ( (2/8)*(idx_j==0) + (1/8)*(idx_j==1) + (-1/8)*(idx_j==3) + (-2/8)*(idx_j==4)); delta = 2*Math.PI * (idx_i/8)
-    //console.log("angles: ",theta, delta)
-  })
+  cc = document.getElementById("renderer")
 
   canvas = document.getElementById("canvas")
   ctx = canvas.getContext("2d")  
+
+  canvasMin = document.getElementById("canvasMiniatures")
+  ctxMin = canvasMin.getContext("2d")  
+
   // Caméra 
   camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 1000 );
   camera.position.x = 2;
@@ -125,8 +71,16 @@ function init(){
   //camera.lookAt (new THREE.Vector3(0,0,0))
   scene = new THREE.Scene();
   scene.add(camera)
-  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer = new THREE.WebGLRenderer( { 
+    antialias: true,
+    preserveDrawingBuffer: true } );
+
   renderer.setSize(W_3D , H_3D);
+  old_renderer = document.getElementById('renderer')
+  if (old_renderer!=  null){
+    old_renderer.parentElement.removeChild(old_renderer)
+  }
+  renderer.domElement.id = 'renderer'
   document.body.appendChild( renderer.domElement );
   controls = new THREE.OrbitControls( camera );
   controls.enableZoom = false;
@@ -156,6 +110,94 @@ function init(){
   dirlight_dessous.position.set(-1, -2, 0);
   scene.add(dirlight_dessous);
 
+  // Data 3D
+  obj_file = ['dragon_update_user_study.obj', 'camel_update_user_study_normed.obj', 'gorgoile_update_user_study_centered_normed.obj']
+  const objLoader = new THREE.OBJLoader2();
+  objLoader.load('https://raw.githubusercontent.com/PelissierCombescure/User_study/main/3DMesh/'+obj_file[idx_mesh], (event) => {
+      const root = event.detail.loaderRootNode;
+      scene.add(root);
+      });
+  mesh_courant = obj_file[idx_mesh].split('_')[0]
+  choix_courant['obj_file'] = obj_file[idx_mesh]
+  choix_courant['mesh'] = mesh_courant
+}
+
+function init(){
+  document.addEventListener("keydown", function(event) {
+    switch (event.key){
+      case  'ArrowLeft': 
+        console.log("deplacement K-G")
+        idx_i = (idx_i+1)%8
+        break;
+      case 'ArrowRight' :
+        console.log("deplacement K-D")
+        idx_i = (idx_i+7)%8
+        break;
+      case 'ArrowDown' :
+        console.log("deplacement K-B")
+        idx_j = Math.min(idx_j+1,4) 
+        break;
+      case 'ArrowUp' :
+        console.log("deplacement K-H")  
+        idx_j = Math.max(idx_j-1,0)
+        break;
+      // valider pose
+      case ' ' :
+        // on regarde si la pose sélectionnée n'a pas déjà été choisie avant
+        pose_deja_choisie(liste_poses, idx_i, idx_j)
+        if (deja_choisie){console.log("Cette pose a déjà été sélectionnée.")}
+        // si on a pas encore choisie toutes nos poses, on peut en ajouter
+        if (nb_choix_fait < nb_choix_demande && !(deja_choisie)){
+            liste_poses.push(['choix'+(nb_choix_fait+1),theta, delta, idx_i, idx_j, new Date().getTime() - time_click]) 
+            time_click = new Date().getTime()
+            nb_choix_fait = nb_choix_fait+1 }
+        // plus de choix possible
+        if (nb_choix_fait == nb_choix_demande) {console.log("Tu as déjà fait tes "+nb_choix_demande+" choix.")}
+        break;
+      // retirer
+      case 'Backspace':
+        console.log('bouton retirer')
+        // il y a des poses à retirer
+        if (liste_poses.length > 0){
+          liste_poses.pop()
+          nb_choix_fait = nb_choix_fait-1} 
+        // S'il n'y a pas de pose choisie
+        else {console.log("Il n'y a pas de pose à retirer.")}
+        break;
+      // reintialiser
+      case 'Delete':
+        console.log('bouton retirer')
+        // on reprends au début
+        time_click = new Date().getTime()
+        liste_poses = []
+        nb_choix_fait = 0
+        break;
+      // valider
+      case  'Enter':
+        console.log('bouton valider')
+        if (nb_choix_demande == liste_poses.length){
+            console.log('au suivant')
+            choix_courant['choix_poses'] = liste_poses
+            choix['tache_N'+num_tache] = choix_courant
+            // RAZ
+            choix_courant = {}
+            liste_poses = []
+            nb_choix_fait = 0
+            // Mesh suivant 
+            indice_mesh = indice_mesh + 1
+            num_tache = num_tache+1
+            setUp_3D(indice_mesh)
+        }
+        else{
+            console.log("Tu n'as pas fait tes 3 choix")
+        }
+        break;
+
+    }
+    //theta = 2*Math.PI * ( (2/8)*(idx_j==0) + (1/8)*(idx_j==1) + (-1/8)*(idx_j==3) + (-2/8)*(idx_j==4)); delta = 2*Math.PI * (idx_i/8)
+    //console.log("angles: ",theta, delta)
+  })
+
   // Data 2D
   imgs = {}
   imgs["droite"] = new_image('https://raw.githubusercontent.com/PelissierCombescure/User_study/main/Fleches/fleche_droite.png')//new_image("graphics/fleche_droite.png")
@@ -170,17 +212,6 @@ function init(){
   boutons["choix_pose"] = new_image('https://raw.githubusercontent.com/PelissierCombescure/User_study/main/Boutons/bouton_pose.png')
   boutons["retirer"] = new_image('https://raw.githubusercontent.com/PelissierCombescure/User_study/main/Boutons/bouton_retirer.png')
 
-  // Data 3D
-  obj_file = ['dragon_update_user_study.obj', 'camel_update_user_study_normed.obj', 'gorgoile_update_user_study_centered_normed.obj']
-  const objLoader = new THREE.OBJLoader2();
-  objLoader.load('https://raw.githubusercontent.com/PelissierCombescure/User_study/main/3DMesh/'+obj_file[indice_mesh], (event) => {
-      const root = event.detail.loaderRootNode;
-      scene.add(root);
-      });
-  mesh_courant = obj_file[indice_mesh].split('_')[0]
-  choix_courant['obj_file'] = obj_file[indice_mesh]
-  choix_courant['mesh'] = mesh_courant
-
   // Mouse
   xyMouseMove = {"x": -1, "y": -1}
   xyMouseDown = {"x": -1, "y": -1}
@@ -192,33 +223,59 @@ function init(){
       clicked = true }, false)
   canvas.addEventListener("mouseup", function(event) { xyMouseUp = getMousePos(canvas, event)}, false)
 
-  console.log("fin init")
+   console.log("fin init")
 }
 
 
 function animate() {  
-  // Nettoyage fleche
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-  // Affichage fleche
-  afficher_fleche(imgs, dy, scale_fleche)
-  // affichage de sboutons
-  afficher_bouton(boutons, imgs, scale_bouton, scale_fleche)
-  // traitement fleche (surval + click)
-  traitement_fleche(imgs)
-  // traitement bouton
-  traitement_bouton(boutons)
-  // affichage 3D
-  renderer.render( scene, camera );
-  // Boucle sur animate
-  requestAnimationFrame( animate );
-  // clicked 
-  clicked = false
-  which_clicked = -1
+  if (num_tache <= nb_mesh){
+    // Variable pour les fonctions
+    init_variable_fonction(boutons, imgs)
+    // Nettoyage fleche
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // progress bar
+    progress_bar(num_tache, nb_mesh)
+    // Affichage fleche
+    afficher_fleche(imgs)
+    // affichage de sboutons
+    afficher_bouton(boutons)
+    if (cc === null) {
+      cc = document.getElementById("renderer")
+    }  
+    // traitement fleche (surval + click)
+    traitement_fleche()
+    // traitement bouton
+    traitement_bouton()
+    // afficher + maj du recap de pose choisie : affichage des vue des poses
+    afficher_recap(imgs)
+    // affichage 3D
+    renderer.render( scene, camera );
+    // Boucle sur animate
+    requestAnimationFrame( animate );
 
-  ctx.strokeStyle = "rgb(255, 255, 255)" // Pour que le contour soit rouge
-  ctx.fillStyle = "rgb(255, 255, 255)" // Pour que l'intérieur soit bleu
-  ctx.font = "bold 22pt Courier";
-  ctx.fillText(liste_poses.length, W_3D+10, 100)
+    // Les poses choisies sont grisées
+    bloquer_pose(liste_poses)
+
+    // Texte
+    ctx.strokeStyle = "rgb(255, 255, 255)" // Pour que le contour soit rouge
+    ctx.fillStyle = "rgb(255, 255, 255)" // Pour que l'intérieur soit bleu
+    ctx.font = "bold 18pt Courier";
+    ctx.fillText("Nb choix fait : "+liste_poses.length+","+nb_choix_fait, W_3D-300, H_3D)
+
+    // RAZ 
+    clicked = false
+    which_clicked_fleche = -1
+    which_clicked_bouton = -1
+  }
+  else {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    console.log('fini !!!')
+    draw_rectangle(0,0,canvas.width, canvas.height, "rgb(34,34,34)", 1)
+    ctx.strokeStyle = "rgb(255, 255, 255)" // Pour que le contour soit rouge
+    ctx.fillStyle = "rgb(255, 255, 255)" // Pour que l'intérieur soit bleu
+    ctx.font = "bold 58pt Courier";
+    ctx.fillText("FIN", W_3D-300, H_3D-100)
+  }
 
   
 
